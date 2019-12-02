bc15c4f799978905ecdece14131db78ce9c60240
Komputer <Daniel200825@wp.pl>
2019-12-02 05:35:47 +0100

    k

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,10 +1,9 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -19,6 +18,7 @@ public class Ui extends Application {
     public void prepareLoginScene(Scene scene){
         login = new BorderPane();
         login.setPrefSize(1000, 800);
+        login.setPadding(new Insets(300,300,300,300));
         scene.setRoot(login);
         VBox vBox = new VBox();
         Label loginLabel = new Label("login: ");
@@ -58,18 +58,88 @@ public class Ui extends Application {
         HBox hBox = new HBox();
         Button button = new Button("lista kursów");
         Button button1 = new Button("kup bilet");
-        Button button2 = new Button("cos jeszcze");
+        //Button button2 = new Button("cos jeszcze");
         hBox.getChildren().add(button);
         hBox.getChildren().add(button1);
-        hBox.getChildren().add(button2);
+        //hBox.getChildren().add(button2);
         user.setTop(hBox);
+
+        VBox center = new VBox();
+        Label label = new Label("Lista kursów: ");
+        center.getChildren().add(label);
+        user.setCenter(center);
+        VBox left = new VBox();
+        left.setPrefSize(200,200);
+        Label label1 = new Label("kurs z: ");
+        Label label2 = new Label("kurs do: ");
+        TextArea area = new TextArea();
+        area.setPrefSize(200,100);
+        TextArea area1 = new TextArea();
+        area1.setPrefSize(200,100);
+        left.getChildren().add(label1);
+        left.getChildren().add(area);
+        left.getChildren().add(label2);
+        left.getChildren().add(area1);
+        user.setLeft(left);
         //user.setCenter(button);
     }
     public void prepareAdminScene(){
         admin = new BorderPane();
         admin.setPrefSize(1000, 800);
-        Button button = new Button("b");
-        admin.setCenter(button);
+
+        TableView tableView = new TableView();
+        TableColumn one = new TableColumn("Imie");
+        TableColumn two = new TableColumn("Nazwisko");
+        TableColumn three = new TableColumn("Pensja");
+        TableColumn four = new TableColumn("Autokar");
+        tableView.getColumns().addAll(one,two,three,four);
+
+        VBox vBox = new VBox();
+        /*vBox.setPadding(new Insets(200,0,0,0));
+        Label from = new Label("Trasa z: ");
+        TextArea a = new TextArea();
+        a.setPrefSize(100,100);
+        Label to = new Label("Trasa do: ");
+        TextArea b = new TextArea();
+        b.setPrefSize(100,100);
+        vBox.getChildren().addAll(from,a,to,b);*/
+        vBox.setVisible(false);
+
+        MenuBar menuBar = new MenuBar();
+        Menu edit = new Menu("Edycja");
+        MenuItem add = new MenuItem("Dodaj rekord");
+        MenuItem delete = new MenuItem("Usuń rekord");
+        MenuItem save = new MenuItem("Zapisz zmiany");
+        MenuItem discard = new MenuItem("Odrzuć zmiany");
+        edit.getItems().addAll(add,delete,save,discard);
+
+        Menu navigate = new Menu("Nawigacja");
+        MenuItem kurs = new MenuItem("Lista kursów");
+        kurs.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        });
+        MenuItem drivers = new MenuItem("Lista kierowców");
+        drivers.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        });
+        navigate.getItems().addAll(kurs,drivers);
+
+        Menu raports = new Menu("Generacja raportów");
+        MenuItem week = new MenuItem("Tygodniowy");
+        MenuItem month = new MenuItem("Miesięczny");
+        raports.getItems().addAll(week,month);
+
+        menuBar.getMenus().addAll(edit,navigate,raports);
+
+        admin.setLeft(vBox);
+        admin.setCenter(tableView);
+        admin.setTop(menuBar);
     }
     @Override
     public void start(Stage primaryStage) {
@@ -90,7 +160,7 @@ public class Ui extends Application {
         borderPane.setCenter(button);
         borderPane.setPrefSize(1000, 800);
         //Scene scene = new Scene(borderPane);
-        primaryStage.setTitle("ZSI");
+        primaryStage.setTitle("SZRBD");
         primaryStage.setScene(scene);
         primaryStage.show();
         button1.setOnAction(new EventHandler<ActionEvent>() {
